e2437fe4c9dca641766485bb9e2b5e1f91372f94
Tetsuro Sakamoto <testuro53@gmail.com>
2024-08-29 07:13:33 +0900

    Change note position numbering

--- src/note.test.ts
+++ src/note.test.ts
@@ -1,31 +1,44 @@
-import { Note } from "./note";
+import { Accidental, Note, Octave, Position } from "./note";
 
 test("construction", () => {
   new Note(0, 0, 0); // C4
-  new Note(0, 1, 0); // C#4, D-4
-  new Note(0, 2, 0); // D4
-  new Note(0, 3, 0); // D#4, E-4
-  new Note(0, 4, 0); // E4
-  new Note(0, 5, 0); // F4
-  new Note(0, 6, 0); // F#4, G-4
-  new Note(0, 7, 0); // G4
-  new Note(0, 8, 0); // G#4, A-4
-  new Note(0, 9, 0); // A4
-  new Note(0, 10, 0); // A#4, B-4
-  new Note(0, 11, 0); // B4
+  new Note(0, 0, +1); // C#4
+  new Note(0, 1, -1); // D-4
+  new Note(0, 1, 0); // D4
+  new Note(0, 1, +1); // D#4
+  new Note(0, 2, -1); // E-4
+  new Note(0, 2, 0); // E4
+  new Note(0, 3, 0); // F4
+  new Note(0, 3, +1); // F#4
+  new Note(0, 4, -1); // G-4
+  new Note(0, 4, 0); // G4
+  new Note(0, 4, +1); // G#4
+  new Note(0, 5, -1); // A-4
+  new Note(0, 5, 0); // A4
+  new Note(0, 5, +1); // A#4
+  new Note(0, 6, -1); // B-4
+  new Note(0, 6, 0); // B4
 
-  new Note(-1, 11, 1); // B#3
-  new Note(-1, 11, -1); // B-3
-  new Note(0, 11, 1); // B#4
-  new Note(0, 11, -1); // B-4
-  new Note(1, 11, 1); // B#5
-  new Note(1, 11, -1); // B-5
+  new Note(0, 0, +2); // C##4
+  new Note(0, 0, -2); // C--4
 
-  new Note(+3, 1, 0); // C7
-  new Note(+2, 1, 0); // C6
-  new Note(+1, 1, 0); // C5
-  new Note(-1, 1, 0); // C3
-  new Note(-2, 1, 0); // C2
-  new Note(-3, 1, 0); // C1
-  new Note(-4, 1, 0); // C0
+  new Note(-4, 0, 0); // C0
+  new Note(-3, 0, 0); // C1
+  new Note(-2, 0, 0); // C2
+  new Note(-1, 0, 0); // C3
+  new Note(+1, 0, 0); // C5
+  new Note(+2, 0, 0); // C6
+  new Note(+3, 0, 0); // C7
+
+  for (let octave = -4; octave <= 3; octave++) {
+    for (let position = 0; position <= 11; position++) {
+      for (let accidental = -2; accidental <= 2; accidental++) {
+        new Note(
+          octave as Octave,
+          position as Position,
+          accidental as Accidental,
+        );
+      }
+    }
+  }
 });
--- src/note.ts
+++ src/note.ts
@@ -1,9 +1,9 @@
-type Position = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
-type Octave = -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3;
+export type Position = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+export type Octave = -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3;
 
 // Accidental
 // double flat | flat | natural | sharp | double sharp
-type Accidental = -2 | -1 | 0 | 1 | 2;
+export type Accidental = -2 | -1 | 0 | 1 | 2;
 
 export class Note {
   constructor(
