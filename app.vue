1878e0902dcb172e067e75c345a2d783098f9909
monman53 <tetsuro53@gmail.com>
2025-07-09 00:18:54 +0900

    五線譜のテスト

--- app.vue
+++ app.vue
@@ -1,31 +1,6 @@
 <template>
   <div>
-    <h1>BravuraフォントとUnicode文字の表示 (OTF版)</h1>
-    <svg width="200" height="150" viewBox="0 0 200 150">
-        <text x="50" y="100" class="bravura-text">&#xe050;</text>
-    </svg>
-    <p>上記SVG内にUnicode文字<span class="bravura-text">&#xe050;</span>がBravuraフォントで表示されているはずです。</p>
+    <h1>藝大和声I巻</h1>
+    <MainScore/>
   </div>
 </template>
-
-<style>
-    @font-face {
-        font-family: 'Bravura';
-        src: url('Bravura.otf') format('opentype');
-    }
-
-    body {
-        font-family: sans-serif;
-    }
-
-    svg {
-        border: 1px solid #ccc;
-    }
-
-    .bravura-text {
-        font-family: 'Bravura', sans-serif;
-        /* font-size: 80px; */
-        /* fill: black; */
-        fill: red;
-    }
-</style>
---
+++ components/MainScore.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h2>楽譜</h2>
+    <svg width="800" height="200" viewBox="-200 -50 400 100">
+        <text x="5" y="-5" class="bravura-text" fill="red">&#xe050;</text>
+        <line x1="0" y1="0" x2="100" y2="0" stroke="black" />
+        <line x1="0" y1="-5" x2="100" y2="-5" stroke="black" />
+        <line x1="0" y1="-10" x2="100" y2="-10" stroke="black" />
+        <line x1="0" y1="-15" x2="100" y2="-15" stroke="black" />
+        <line x1="0" y1="-20" x2="100" y2="-20" stroke="black" />
+    </svg>
+  </div>
+</template>
+
+<style>
+    @font-face {
+        font-family: 'Bravura';
+        src: url('Bravura.otf') format('opentype');
+    }
+
+    body {
+        font-family: sans-serif;
+    }
+
+    svg {
+        border: 1px solid #ccc;
+    }
+
+    .bravura-text {
+        font-family: 'Bravura', sans-serif;
+        font-size: 20px;
+        /* fill: black; */
+        /* fill: red; */
+    }
+</style>
