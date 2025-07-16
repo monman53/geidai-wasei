3a0a1a3f4eea40fe40f9caca28d1aa875198567d
monman53 <tetsuro53@gmail.com>
2025-07-17 02:54:21 +0900

    Better UI

--- app.vue
+++ app.vue
@@ -26,29 +26,33 @@ const musicEnd = ref(10);
 <template>
   <div class="m-3">
     <h1 class="text-3xl font-bold">藝大和声 I 巻</h1>
-    <label>
-      <input type="radio" v-model="mode" :value="Mode.ProblemEdit" />
-      課題作成
-    </label>
-    <label>
-      <input type="radio" v-model="mode" :value="Mode.Solve" />
-      実施
-    </label>
+    <div class="my-3">
+      <label>
+        <input type="radio" v-model="mode" :value="Mode.ProblemEdit" />
+        課題作成
+      </label>
+      <label>
+        <input type="radio" v-model="mode" :value="Mode.Solve" />
+        実施
+      </label>
+    </div>
     <FullScore :harmonies="harmonies" :mode="mode" :svg-scale="3" />
-    <button
-      @click="
-        () => {
-          mode = Mode.Solve;
-          musics = chordSolver(harmonies, Key.C);
-        }
-      "
-      class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 rounded-lg text-sm px-3 py-2 pointer"
-    >
-      自動実施
-    </button>
+    <div class="my-3">
+      <button
+        @click="
+          () => {
+            mode = Mode.Solve;
+            musics = chordSolver(harmonies, Key.C);
+          }
+        "
+        class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 rounded-lg text-sm px-3 py-2 pointer"
+      >
+        自動実施
+      </button>
+    </div>
     <div v-if="mode === Mode.Solve">
       <div v-for="(music, idx) in musics.slice(musicStart, musicEnd)">
-        {{ musicStart + idx + 1 }} / {{ musics.length }}, Penalty:
+        実施例: {{ musicStart + idx + 1 }} / {{ musics.length }}, Penalty:
         {{ music.penalty }}
         <FullScore
           :harmonies="music.harmonies"
